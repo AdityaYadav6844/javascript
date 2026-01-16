ce6be2f2fc0027a519d615e4acebe82f840fe790
aditya yadav <adiuk07@gmail.com>
2026-01-16 22:26:08 +0530

    Add mini-project1

---
+++ mini-project1/script.js
@@ -0,0 +1,22 @@
+const buttons= document.querySelectorAll('.button')
+const body= document.querySelector('body')
+
+buttons.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click',function(e){
+        switch (e.target.id) {
+            case "grey":
+                
+            case "white":
+                
+            case "blue":
+                
+            case "yellow":
+                body.style.backgroundColor=e.target.id
+                break;
+        
+            default:
+                break;
+        }
+    });
+});
